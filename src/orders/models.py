2519d94d47b9ee4713731bbcc8807b5e249974e1
Denis Voskvitsov <denis.voskvitsov@gmail.com>
2010-04-28 12:51:03 +0400

    repair form + fixes

--- src/orders/models.py
+++ src/orders/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
+from django.forms.forms import BoundField
+__init__temp = BoundField.__init__
+ 
+def __my_init__(self, form, field, name):
+	__init__temp(self, form, field, name)
+	if self.field.required:
+		self.label += u'*'
+ 
+BoundField.__init__ = __my_init__
+
 class RepairRequest(models.Model):
     name = models.CharField(_("Name"), max_length=1000, blank=False, null=False)
     sender = models.EmailField(_("E-mail"),  blank=False, null=False)
