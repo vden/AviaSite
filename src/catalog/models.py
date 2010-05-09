c1340e7a4920c0ea24ca3d7733f3fa2004be684a
Denis Voskvitsov <denis.voskvitsov@gmail.com>
2010-05-10 00:22:12 +0400

    model fix

--- src/catalog/models.py
+++ src/catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Equipment(models.Model):
-	code = models.IntegerField(u"System code", blank=True, null=True)
+	code = models.IntegerField(u"System code", blank=True, null=True, primary_key=True)
 	name = models.CharField(u"KI name", max_length=1024)
 	cipher = models.CharField(u"Cipher", max_length=1024)
 	draw_number = models.CharField(u"Draw number", max_length=1024, blank=True, null=True)
