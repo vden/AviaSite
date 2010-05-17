ae1f712dd30b445ad57b2b65c44e0feaf5417944
Denis Voskvitsov <denis.voskvitsov@gmail.com>
2010-05-18 00:07:16 +0400

    catalog fix

--- src/catalog/views.py
+++ src/catalog/views.py
@@ -23,53 +23,43 @@ def __search_catalog(name, cipher):
 def index(request):
     	name = cipher = u""
 	page = request.GET.get("page", 0)
+	letter = int(request.GET.get("letter", 0))
 	try: page = int(page)
 	except: page = 0
+
+	rx = (	ur'^(А|Б|В|Г|Д|Е|а|б|в|г|д|е)',
+		ur'^(Ж|З|И|К|Л|ж|з|и|к|л)', 
+		ur'^(М|Н|О|П|Р|С|Т|м|н|о|п|р|с|т)',
+		ur'^(У|Ф|Х|Ц|Ч|Ш|Щ|Э|Ю|Я|у|ф|х|ц|ч|ш|щ|э|ю|я)'  )
 
 	if request.method == "POST":
 		name = request.POST.get("name", None)
 		cipher = request.POST.get("cipher", None)
 
-		res = __search_catalog(name, cipher)
+		res = __search_catalog(name, cipher).order_by('name')
 		request.session["catalog_POST"] = (name, cipher)
+	elif letter > 0:
+		letter = rx[letter-1]
+		post = request.session.get("catalog_POST", [])
+		if post:
+			res = __search_catalog(*post).filter(name__regex = letter).order_by('name')
+			name, cipher = post
+		else:
+			res = Equipment.objects.filter(name__regex = letter).order_by('name')
 	else:	
 		post = request.session.get("catalog_POST", [])
 		if page > 0:
 			if post:
-				res = __search_catalog(*post)
+				res = __search_catalog(*post).order_by('name')
 				name, cipher = post
 			else:
 				res = Equipment.objects.order_by('name') 
 		else:        
 			if post: del request.session["catalog_POST"]
 			res = Equipment.objects.order_by('name') 
-	rx = {	
-		ur'^[А-Е]$': "a",
-		ur'^[Ж-Л]$': "zh",
-		ur'^[М-Т]$': "m",
-		ur'^[У-Я]$': "u"
-	     }
-
-	rx_page = {'a': 1, 'zh': 1, 'm': 1, 'u': 1}
-	rx_page_set = []
-
-	current_num = 0
-	for r in res:
-		l = r.name[0].upper()
-	
-		for k in rx.keys():
-			if re.match(k, l):	
-				r.set_menu_anchor(rx[k])
-				if not rx[k] in rx_page_set:
-					rx_page[rx[k]] = current_num // settings.CATALOG_OBJECTS_PER_PAGE + 1
-					rx_page_set.append(rx[k])
-				break
-		current_num += 1
 
 	page = page == 0 and 1 or page
 	paginator = Paginator(res, settings.CATALOG_OBJECTS_PER_PAGE)
 	res = paginator.page(page)	
 
-	
-
-	return render_to_response("catalog/index.html", {'lst': res, 'name': name, 'cipher': cipher, 'pages': rx_page}, context_instance=RequestContext(request))
+	return render_to_response("catalog/index.html", {'lst': res, 'name': name, 'cipher': cipher}, context_instance=RequestContext(request))
